c505a0471f9202c2669a3ad02d6bbb7eaec66200
takuan1108 <flying.bed.in.dream@gmail.com>
2019-02-13 12:20:18 +0900

    Install devise and add views

---
+++ db/migrate/20190213024130_devise_create_rails.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class DeviseCreateRails < ActiveRecord::Migration[5.0]
+  def change
+    create_table :rails do |t|
+      t.string :name,               null: false, unique: true
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+      t.string :g
+      t.string :devise
+      t.string :User
+
+      t.timestamps null: false
+    end
+
+    add_index :rails, :email,                unique: true
+    add_index :rails, :reset_password_token, unique: true
+    # add_index :rails, :confirmation_token,   unique: true
+    # add_index :rails, :unlock_token,         unique: true
+  end
+end
